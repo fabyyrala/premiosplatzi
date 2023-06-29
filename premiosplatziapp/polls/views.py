da62751abb8ddd7553359907f03447017a33a8e5
fabyyrala <fabiyrala@gmail.com>
2023-06-29 14:53:44 -0300

    Cambio

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, i hate django.")
+    return HttpResponse("Hello, i hate django")
 
 
 # Create your views here.
